c257dd00779cd2dd24c1e68a8b95d73836027fe5
bmikolajczak <br.mikolajczak@gmail.com>
2021-09-11 23:59:04 +0200

    adding details

---
+++ src/components/Detail.vue
@@ -0,0 +1,29 @@
+<template>
+  <div v-show="detailShow">
+      <button type="button">Close me</button>
+      <p>Overview: {{result.overview}}</p>
+      <p>Date: {{result.release_date}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Details',
+    props: ['result','detailShow'],
+    data(){
+        return{
+
+       }
+    },
+    methods:{
+        detailHide(){
+
+        }
+    }
+
+}
+</script>
+
+<style>
+
+</style>
--- src/components/Search.vue
+++ src/components/Search.vue
@@ -2,28 +2,39 @@
   <div class="search">
       <h1>Search</h1>
       <input type="text" v-model="query" @keyup="getResult(query)">
-      <div v-for="result in results" :key="result.id">
+      <div v-for="result in results" :key="result.id" @click="detailShow=!detailShow">
           <p>{{result.title}}</p>
+          <p>Popularity: {{result.popularity}}</p>
+          <p>Vote Count: {{result.vote_count}}</p>
           <img :src="'http://image.tmdb.org/t/p/w500/'+ result.poster_path" style="width:150px">
+          <app-detail 
+          :result="result" 
+          :detailShow='detailShow'>
+          </app-detail>
       </div>
   </div>
+  
 </template>
 
 <script>
 import axios from 'axios';
+import AppDetail from '@/components/Detail.vue';
 export default {
     name:'search',
+    components:{AppDetail,
+    },
     data(){
         return{
             query: '',
             results: '',
+            detailShow: false,
         }
     },
     methods: {
         getResult(query){
             axios.get(`https://api.themoviedb.org/3/search/movie?api_key=6dcc216133b93aa7fd311eb61b2980ac&query=${query}`)
             .then(response => {this.results = response.data.results});
-            console.log(this.results);
+            console.log(typeof this.results, this.results);
         }
     }
 }
