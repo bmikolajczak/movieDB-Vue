82bc12252aef99763b343be71d1443b9fd19f2f7
bmikolajczak <br.mikolajczak@gmail.com>
2022-02-14 00:43:58 +0100

    removing comments

--- src/components/Search.vue
+++ src/components/Search.vue
@@ -1,156 +1,125 @@
 <template>
   <div class="search">
-      <h1>Search</h1>
-      <input type="text" v-model="query" @keyup="getResult(query)" placeholder="Search for movie">
-      <br>
-      <label for="sort">Sort by:</label>
-      <select name="sort" v-model="sortOption" @change="sortResults()">
-          <option value="alpha">Title</option>
-          <option value="popular">Populatiry</option>
-          <option value="vote_count">Vote Count</option>
-      </select>
-      
+    <h1>Search</h1>
+    <input type="text" v-model="query" @keyup="getResult(query)" placeholder="Search for movie" />
+    <br />
+    <label for="sort">Sort by:</label>
+    <select name="sort" v-model="sortOption" @change="sortResults()">
+      <option value="alpha">Title</option>
+      <option value="popular">Populatiry</option>
+      <option value="vote_count">Vote Count</option>
+    </select>
+
     <section class="movie-list">
-        <div class="movie-item"
-        v-for="result in results" :key="result.id" @click="showInfo(result)">
-            <img :src="'http://image.tmdb.org/t/p/w500/'+ result.poster_path" style="width:150px">
-            <p>{{result.title}}</p>
-            <p>Popularity: {{result.popularity}}</p>
-            <p>Vote Count: {{result.vote_count}}</p>  
-        </div>
+      <div class="movie-item" v-for="result in results" :key="result.id" @click="showInfo(result)">
+        <img :src="'http://image.tmdb.org/t/p/w500/' + result.poster_path" style="width:150px" />
+        <p>{{ result.title }}</p>
+        <p>Popularity: {{ result.popularity }}</p>
+        <p>Vote Count: {{ result.vote_count }}</p>
+      </div>
     </section>
-    </div>
-  <app-detail :movie="currentMovie" :movieGenre="movieGenre"/>
+  </div>
+  <app-detail :movie="currentMovie" :movieGenre="movieGenre" />
 </template>
 
 <script>
-import axios from 'axios';
-import AppDetail from '@/components/Detail.vue';
-import {mapState, mapMutations} from 'vuex'
+import axios from "axios";
+import AppDetail from "@/components/Detail.vue";
+import { mapState, mapMutations } from "vuex";
 export default {
-    name:'search',
-    components:{AppDetail,
+  name: "search",
+  components: { AppDetail },
+  data() {
+    return {
+      apiKey: "6dcc216133b93aa7fd311eb61b2980ac",
+      query: "",
+      results: "",
+      movieGenre: "",
+      sortOption: "popular",
+      currentMovie: {},
+    };
+  },
+  computed: {
+    ...mapState(["showDetail"]),
+  },
+  methods: {
+    ...mapMutations(["toggleDetail"]),
+    getResult(query) {
+      axios.get(`https://api.themoviedb.org/3/search/movie?api_key=${this.apiKey}&query=${query}`).then((response) => {
+        this.results = response.data.results;
+
+        // results sorting
+        this.sortResults();
+      });
+      console.log(this.results);
     },
-    data(){
-        return{
-            query: '',
-            results: '',
-            movieGenre: '',
-            sortOption:'popular',
-            currentMovie:{},
-        }
+    showInfo(result) {
+      this.currentMovie = result;
+      console.log(this.currentMovie.id, this.currentMovie.overview);
+      console.log(`Modal status: ${this.$store.state.showDetail}`);
+
+      axios
+        .get(`https://api.themoviedb.org/3/movie/${this.currentMovie.id}?api_key=6dcc216133b93aa7fd311eb61b2980ac`)
+        .then((response) => {
+          console.log(response.status, response.data.genres);
+          this.movieGenre = response.data.genres;
+          this.movieGenre.forEach((elem) => console.log(elem.name));
+        })
+        .catch((err) => console.log(err));
+
+      this.$store.commit("toggleDetail");
     },
-    computed:{
-        ...mapState(['showDetail']),
+    sortResults() {
+      if (this.sortOption == "alpha") {
+        //alphapetical sort
+        this.results.sort((a, b) => {
+          let aTitle = a.title.toLowerCase();
+          let bTitle = b.title.toLowerCase();
+          if (aTitle < bTitle) {
+            return -1;
+          }
+          if (aTitle > bTitle) {
+            return 1;
+          }
+          return 0;
+        });
+      }
+
+      if (this.sortOption == "popular") {
+        // sort by popularity
+        this.results.sort((a, b) => b.popularity - a.popularity);
+      }
+
+      if (this.sortOption == "vote_count") {
+        // sort by vote count
+        //! fix
+        console.log("vote_count");
+        this.results.sort((a, b) => {
+          b.vote_count - a.vote_count;
+        });
+      }
     },
-    methods: {
-        ...mapMutations(['toggleDetail']),
-        getResult(query){
-            axios.get(`https://api.themoviedb.org/3/search/movie?api_key=6dcc216133b93aa7fd311eb61b2980ac&query=${query}`)
-            .then(response => {
-                this.results = response.data.results;
-                
-                // results sorting
-                this.sortResults();
-                // if(this.sortOption=='alpha')
-                // {
-                //     //alphapetical sort
-                //     this.results.sort((a,b)=>{
-                //         let aTitle= a.title.toLowerCase();
-                //         let bTitle= b.title.toLowerCase();
-                //         if(aTitle < bTitle) {
-                //             return -1;
-                //         }
-                //         if(aTitle > bTitle) {
-                //             return 1;
-                //         }
-                //         return 0;
-                //     }) 
-                // }
-                // if(this.sortOption=='popular')
-                // {
-                //     // sort by popularity
-                //     this.results.sort((a,b)=> b.popularity-a.popularity);
-                // }
-                // if(this.sortOption=='vote_count')
-                // {
-                //     // sort by vote count
-                //     this.results.sort((a,b)=>{
-                //         b.vote_count-a.vote_count;
-                //     });
-                // }
-                
-                });
-            console.log(this.results);
-        },
-        showInfo(result){
-            this.currentMovie=result;
-            console.log(this.currentMovie.id, this.currentMovie.overview);
-            console.log(`Modal status: ${this.$store.state.showDetail}`);
-            
-            axios.get(`https://api.themoviedb.org/3/movie/${this.currentMovie.id}?api_key=6dcc216133b93aa7fd311eb61b2980ac`)
-            .then(response=>{
-                console.log(response.status, response.data.genres)
-                this.movieGenre=response.data.genres;
-                this.movieGenre.forEach(elem=>console.log(elem.name))})
-            .catch(err=>console.log(err));
-
-            this.$store.commit('toggleDetail');
-        },
-        sortResults(){
-
-            if(this.sortOption=='alpha')
-                {
-                    //alphapetical sort
-                    this.results.sort((a,b)=>{
-                        let aTitle= a.title.toLowerCase();
-                        let bTitle= b.title.toLowerCase();
-                        if(aTitle < bTitle) {
-                            return -1;
-                        }
-                        if(aTitle > bTitle) {
-                            return 1;
-                        }
-                        return 0;
-                    }) 
-                }
-
-                if(this.sortOption=='popular')
-                {
-                    // sort by popularity
-                    this.results.sort((a,b)=> b.popularity-a.popularity);
-                }
-
-                if(this.sortOption=='vote_count')
-                {
-                    // sort by vote count
-                    this.results.sort((a,b)=>{
-                        b.vote_count-a.vote_count;
-                    });
-                }
-        },
-
-    }
-}
+  },
+};
 </script>
 
-<style >
-.movie-list{
-    padding: 1rem;
-    display: flex;
-    flex-wrap: wrap;
-    justify-content: center;
-    align-items: center;
-    width: 100vw;
+<style>
+.movie-list {
+  padding: 1rem;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+  align-items: center;
+  width: 100vw;
 }
-.movie-item{
-    background: rgb(21, 82, 54);
-    width: 20rem;
-    max-height: 50rem;
-    margin: 0 .2rem .3rem 0;
-    border-radius: .2rem;
+.movie-item {
+  background: rgb(21, 82, 54);
+  width: 20rem;
+  max-height: 50rem;
+  margin: 0 0.2rem 0.3rem 0;
+  border-radius: 0.2rem;
 }
-.movie-item img{
-    width: 100%;
+.movie-item img {
+  width: 100%;
 }
 </style>
