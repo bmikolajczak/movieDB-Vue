82d1d0e2ead9c266b3129685ad26ef873de15696
bmikolajczak <br.mikolajczak@gmail.com>
2022-03-08 01:12:16 +0100

    about page styling

--- src/App.vue
+++ src/App.vue
@@ -30,6 +30,7 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   /* text-align: center; */
   color: #000000;
+  background: $bg-gradient;
 }
 * {
   margin: 0;
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,10 +1,27 @@
 <template>
-  <h1>About page</h1>
-  <h2>THIS IS ABOUT PAGE</h2>
+  <article class="about">
+    <h1>The Movie Database (TMDB) App built with Vue and Firebase</h1>
+    <div class="logos">
+      <img src="../assets/logo.png" alt="VUE" />
+      <img src="../assets/tmdb.png" alt="TMDB" />
+      <img src="../assets/firebase.png" alt="Firebase" />
+    </div>
+    <div class="paragraphs">
+      <p>Welcome to this simple Vue app that I built that takes data from TMDB to view it's resources.</p>
+      <p>
+        I also implemented simple Firebase functionality that allows users to create accounts and save movies to
+        favourites in their profile.
+      </p>
+    </div>
+  </article>
 </template>
 
 <script>
 export default {};
 </script>
 
-<style></style>
+<style scoped lang="scss">
+@import "../assets/scss/About.scss";
+.about {
+}
+</style>
